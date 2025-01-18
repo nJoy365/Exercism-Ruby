fd67a59fae6c0c716476fde6594f02da62e5b6b5
nJoy365 <alan.przybylowicz@gmail.com>
2025-01-18 14:36:53 +0000

    Complete exercise

--- eliuds-eggs/eliuds_eggs.rb
+++ eliuds-eggs/eliuds_eggs.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'Eliuds Eggs' exercise in this file. Make the tests in
-`eliuds_eggs_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/eliuds-eggs` directory.
-=end
+class EliudsEggs
+  def self.egg_count(n)
+    n.to_s(2).count('1')
+  end
+end
